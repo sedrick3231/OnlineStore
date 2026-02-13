ac93907e15bd018e3d43f1fd4a965494e5fb32d7
saadullah <saadnawaz320@gmail.com>
2026-02-13 15:21:56 -0700

    Online Shoping Store

--- Backend/controllers/emailServices.js
+++ Backend/controllers/emailServices.js
@@ -79,10 +79,10 @@ export const verifyUserOTP = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 day
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-       domain:  process.env.NODE_ENV === "production" ? ".up.railway.app": "",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: true, // Always use secure in production
+      sameSite: "None", // Allow cross-domain cookies
+      path: "/",
     });
     res
       .status(200)
